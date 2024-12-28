74582b26ab13347257b39c354345256531ac0fd1
Gerald Young <gerald.young@wirelesszt.com>
2024-12-28 06:00:23 -0500

    First working implementation.

--- main.cpp
+++ main.cpp
@@ -1,42 +1,24 @@
-// Dear ImGui: standalone example application for SDL2 + OpenGL
-// (SDL is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
-
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
-
-#include "imgui.h"
-#include "imgui_impl_sdl2.h"
-#include "imgui_impl_opengl3.h"
-#include <stdio.h>
-#define SDL_MAIN_HANDLED
-#include <SDL.h>
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-#include <SDL_opengles2.h>
-#else
-#define GL_GLEXT_PROTOTYPES 1
-#include <SDL_opengl.h>
-#endif
-
-// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
-#ifdef __EMSCRIPTEN__
-#include "../libs/emscripten/emscripten_mainloop_stub.h"
-#endif
-
-#include "osc/OscReceivedElements.h"
-#include "osc/OscPacketListener.h"
-
-#include "ip/UdpSocket.h"
+/* 
+    Example of two different ways to process received OSC messages using oscpack.
+    Receives the messages from the SimpleSend.cpp example.
+*/
 
 #include <iostream>
-#include <mutex>
-#include <thread>
-#include <map>
-#include <vector>
+#include <cstring>
 #include <memory>
-#include <filesystem>
+
+#if defined(__BORLANDC__) // workaround for BCB4 release build intrinsics bug
+namespace std {
+using ::__strcmp__;  // avoid error: E2316 '__strcmp__' is not a member of 'std'.
+}
+#endif
+
+#include "osc/OscPacketListener.h"
+#include "osc/OscOutboundPacketStream.h"
+#include "ip/UdpSocket.h"
+
+// OpenVR to interact with VR
+#include <openvr.h>
 
 #define ADDRESS "127.0.0.1"
 #define PORT 9000
@@ -45,275 +27,110 @@
 
 #define OUTPUT_BUFFER_SIZE 1024
 
-std::mutex mutex;
-std::map<std::string, std::vector<float> > values;
-UdpListeningReceiveSocket* oscsocket = nullptr;
-int listen_port = LISTEN_PORT;
-bool serverStarted = false;
+using namespace vr;
 
+namespace vr {
+    static const char * const k_pch_VRChat_Section = "steam.app.438100";
+}
 
-char pBuf[256];
-size_t len = sizeof(pBuf);
+static bool isVRValid = false;
+static float osc_scale_min = 0.5034f;
+static float osc_scale_base = 1.0f;
+static float osc_scale_max = 9.0f;
 
-#ifdef __linux__
-#include <unistd.h>
-#include <algorithm>
-#elif _WIN32
-#include <windows.h>
-#else
-#error Unsupported operating system
-#endif
-
-class MessageListener : public osc::OscPacketListener {
-protected:
-    virtual void ProcessMessage(const osc::ReceivedMessage& m, const IpEndpointName& remoteEndpoint)
+float mapScale(float value)
+{
+    if( value <= 0.5f )
     {
-        (void) remoteEndpoint; // suppress unused parameter warning
+        float t = 2 * value;
+        return (1.0f - t) * osc_scale_min + t * osc_scale_base;
+    }
+    else
+    {
+        float t = 2 * (value - 0.5f);
+        return (1.0f - t) * osc_scale_base + t * osc_scale_max;
+    }
+}
+
+class PingHandler : public osc::OscPacketListener {
+protected:
+
+    virtual void ProcessMessage(const osc::ReceivedMessage& m,
+                                const IpEndpointName& remoteEndpoint)
+    {
         try{
-            std::string address = m.AddressPattern();
-            osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
-            float value = 0.0;
-            args >> value >> osc::EndMessage;
-            {
-                const std::lock_guard<std::mutex> lock(mutex);
-                if (address == "/avatar/parameters/SizeOSC_Scale_Puppet") {
-                    UdpTransmitSocket transmitSocket(IpEndpointName(ADDRESS, PORT));
+            // example of parsing single messages. osc::OsckPacketListener
+            // handles the bundle traversal.
+            
+            if( std::strcmp( m.AddressPattern(), "/avatar/parameters/SizeOSC_Scale_Puppet" ) == 0 ){
+                osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
 
-                    char buffer[OUTPUT_BUFFER_SIZE];
-                    osc::OutboundPacketStream p(buffer, OUTPUT_BUFFER_SIZE);
+                struct timespec clientSendTime, serverReceiveTime;
+                clock_gettime(CLOCK_MONOTONIC, &serverReceiveTime);
 
-                    p << osc::BeginBundleImmediate
-                        << osc::BeginMessage( "/avatar/parameters/SizeOSC_Scale" )
-                        << value << osc::EndMessage
-                        << osc::EndBundle;
-                }
+                float value = 0.0f;
 
-                if (values.contains(address)) {
-                    auto &v = values[address];
-                    v[1] = value;
-                    if (v[0] > value) {
-                        v[0] = value;
-                    }
-                    if (v[2] < value) {
-                        v[2] = value;
-                    }
-                } else {
-                    values[address] = std::vector<float>{value, value, value};
-                }
+                args >> value >> osc::EndMessage;
+
+                std::cout << "SizeOSC_Scale_Puppet received: " << value << " sending: " << (value * 2) - 1.0f << " VR: " << 1/mapScale(value) << std::endl;
+
+                if (isVRValid)
+                    vr::VRSettings()->SetFloat(vr::k_pch_VRChat_Section, vr::k_pch_SteamVR_WorldScale_Float, 1/mapScale(value));
+
+                char buffer[OUTPUT_BUFFER_SIZE];
+                osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
+
+                p << osc::BeginBundleImmediate
+                    << osc::BeginMessage( "/avatar/parameters/SizeOSC_Scale" ) 
+                    << (value * 2 - 1.0f) << osc::EndMessage
+                    << osc::EndBundle;
+
+                UdpTransmitSocket transmitSocket(IpEndpointName(ADDRESS, PORT));
+                transmitSocket.Send( p.Data(), p.Size() );
+
             }
         }catch( osc::Exception& e ){
+            // any parsing errors such as unexpected argument types, or 
+            // missing arguments get thrown as exceptions.
             std::cout << "error while parsing message: "
                 << m.AddressPattern() << ": " << e.what() << "\n";
         }
     }
 };
 
-void OscThread()
+void shutdown_vr(vr::IVRSystem *_system)
 {
-    MessageListener listener;
-    UdpListeningReceiveSocket s(IpEndpointName(IpEndpointName::ANY_ADDRESS, listen_port), &listener);
-    oscsocket = &s;
-    s.RunUntilSigInt();
+    vr::VR_Shutdown();
 }
 
-// Main code
-int main(int, char**)
+
+int main(int argc, char* argv[])
 {
-    // Setup SDL
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
+    (void) argc; // suppress unused parameter warnings
+    (void) argv; // suppress unused parameter warnings
+
+    PingHandler listener;
+    UdpListeningReceiveSocket s(
+            IpEndpointName( IpEndpointName::ANY_ADDRESS, LISTEN_PORT ),
+            &listener );
+
+    EVRInitError init_error;
+    std::unique_ptr<IVRSystem, decltype(&shutdown_vr)>
+        system(VR_Init(&init_error, VRApplication_Background), &shutdown_vr);
+    if (init_error)
     {
-        printf("Error: %s\n", SDL_GetError());
-        return -1;
+        std::cout << VR_GetVRInitErrorAsEnglishDescription(init_error) << std::endl;
+        isVRValid = false;
+    }
+    else
+    {
+        isVRValid = true;
     }
 
-    std::unique_ptr<std::thread> oscThread;
+    std::cout << "press ctrl-c to end" << std::endl;
 
-#ifdef __linux__
-    int bytes = std::min(static_cast<size_t>(readlink("/proc/self/exe", pBuf, len)), len - 1);
-    if(bytes >= 0)
-	    pBuf[bytes] = '\0';
-#elif _WIN32
-    int bytes = GetModuleFileName(NULL, pBuf, len);
-#else
-#error Unsupported operating system
-#endif
-    std::filesystem::path iniPath(pBuf);
-    iniPath.replace_filename("imgui.ini");
-    std::string iniFilename = iniPath.string();
-
-    // Decide GL+GLSL versions
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-    // GL ES 2.0 + GLSL 100
-    const char* glsl_version = "#version 100";
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-#elif defined(__APPLE__)
-    // GL 3.2 Core + GLSL 150
-    const char* glsl_version = "#version 150";
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-#else
-    // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-#endif
-
-    // From 2.0.18: Enable native IME.
-#ifdef SDL_HINT_IME_SHOW_UI
-    SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
-#endif
-
-    // Create window with graphics context
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Window* window = SDL_CreateWindow("OSC calibration tool", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
-    if (window == nullptr)
-    {
-        printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
-        return -1;
-    }
-
-    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-    SDL_GL_MakeCurrent(window, gl_context);
-    SDL_GL_SetSwapInterval(1); // Enable vsync
-
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-    io.IniFilename = iniFilename.c_str();
-
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
-
-    // Setup Platform/Renderer backends
-    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
-    ImGui_ImplOpenGL3_Init(glsl_version);
-
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    // - Our Emscripten build process allows embedding fonts to be accessible at runtime from the "fonts/" folder. See Makefile.emscripten for details.
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != nullptr);
-
-    // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-    // Main loop
-    bool done = false;
-#ifdef __EMSCRIPTEN__
-    // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
-    // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
-    io.IniFilename = nullptr;
-    EMSCRIPTEN_MAINLOOP_BEGIN
-#else
-    while (!done)
-#endif
-    {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            ImGui_ImplSDL2_ProcessEvent(&event);
-            if (event.type == SDL_QUIT)
-                done = true;
-            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
-                done = true;
-        }
-
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplSDL2_NewFrame();
-        ImGui::NewFrame();
-
-        {
-            ImGui::Begin("OSC server");
-
-            ImGui::Text("Status: %s", serverStarted ? "Started" : "Stopped");
-            ImGui::InputInt("Port", &listen_port);
-            if (ImGui::Button(serverStarted ? "Stop" : "Start")) {
-                if (serverStarted) {
-                    if (oscsocket)
-                        oscsocket->AsynchronousBreak();
-                    if (oscThread)
-                        oscThread->join();
-                    oscThread.release();
-                    serverStarted = false;
-                } else {
-                    oscThread.reset(new std::thread(&OscThread));
-                    serverStarted = true;
-                }
-            }
-
-            ImGui::End();
-        }
-
-        {
-            ImGui::Begin("OSC values");
-            for (auto& x : values)
-            {
-                ImGui::SliderFloat3(x.first.c_str(), x.second.data(), 0.0f, 1.0f);
-            }
-            ImGui::End();
-        }
-
-        // Rendering
-        ImGui::Render();
-        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        SDL_GL_SwapWindow(window);
-    }
-#ifdef __EMSCRIPTEN__
-    EMSCRIPTEN_MAINLOOP_END;
-#endif
-
-    {
-        if (oscsocket)
-            oscsocket->AsynchronousBreak();
-        if (oscThread)
-            oscThread->join();
-    }
-
-    // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplSDL2_Shutdown();
-    ImGui::DestroyContext();
-
-    SDL_GL_DeleteContext(gl_context);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    s.RunUntilSigInt();
 
     return 0;
 }
+
